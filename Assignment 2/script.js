14a93f043268da3de0b35fef142900ac30550c7f
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-18 18:27:29 +0530

    script.js updated

---
+++ Assignment 2/script.js
@@ -0,0 +1,25 @@
+var start = new Date().getTime();
+        function move(){
+            var left;
+            var top;
+            left = Math.random()*300;
+            top = Math.random()*300;
+            document.getElementById("tester").style.left = left+"px"; ;
+            document.getElementById("tester").style.top = top +"px"; ;
+            document.getElementById("tester").style.display = "block";
+            start = new Date().getTime();
+        }
+        move();
+        document.getElementById("tester").onclick = function(){
+            document.getElementById("tester").style.display = "none";
+            var end = new Date().getTime();
+            var timeTake = (end - start)/1000;
+            if(timeTake < 0.5){
+                alert("You are too fast");
+            }
+            else{
+                alert("Your reaction time is " + timeTake + " seconds");
+            }
+            move();
+        }
+        
