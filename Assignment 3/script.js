64c73b66e5e2a48cdffe2d284317c0692b010498
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-19 18:26:45 +0530

    Confirm Password field added and Number field editted

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -23,8 +23,19 @@ document.getElementById("togglePassword").onclick = function(){
 
 $("#phone").on("input", function () {
     var phoneValue = $(this).val();
-    var phonePattern = /^\d{10}$/; // Only 10 digits allowed
 
+    // Remove any non-digit characters
+    phoneValue = phoneValue.replace(/\D/g, '');
+
+    // Limit to 10 digits
+    if (phoneValue.length > 10) {
+        phoneValue = phoneValue.substring(0, 10);
+    }
+
+    $(this).val(phoneValue); // Set the cleaned value back to input
+
+    // Validate the final value
+    var phonePattern = /^\d{10}$/;
     if (phonePattern.test(phoneValue)) {
         $("#phonepara").css("display", "none");
         $("#phonepara").removeClass("notvalid");
@@ -45,22 +56,37 @@ $("#password").on("input", function () {
     }
 });
 
+$("#conpass").on("input", function () {
+    var passwordValue = $("#password").val();
+    var conpassValue = $(this).val();
+
+    if (conpassValue === passwordValue) {
+        $("#confirmPass").css("display", "none");
+        $("#confirmPass").removeClass("notvalid");
+    } else {
+        $("#confirmPass").addClass("notvalid");
+    }
+});
+
 $("#smtbtn").click(function () {
     event.preventDefault();
     if (
         $("#emailpara").hasClass("notvalid") ||
         $("#phonepara").hasClass("notvalid") ||
-        $("#passwordpara").hasClass("notvalid")
+        $("#passwordpara").hasClass("notvalid")||
+        $("#confirmPass").hasClass("notvalid")
     ) {
         $("#emailpara").addClass("undo");
         $("#phonepara").addClass("undo");
         $("#passwordpara").addClass("undo");
+        $("#confirmPass").addClass("undo");
         $("#unsuccessful").removeClass("undo");
         $("#read").removeClass("undo");
     } else {
          $("#emailpara").addClass("undo");
         $("#phonepara").addClass("undo");
         $("#passwordpara").addClass("undo");
+        $("#confirmPass").addClass("undo");
         $("#successful").removeClass("undo");
     }
 });
@@ -70,6 +96,7 @@ $("#read").click(function () {
     $("#read").addClass("undo");
     $("#emailpara").removeClass("undo");
     $("#phonepara").removeClass("undo");
+    $("#confirmPass").removeClass("undo");
     $("#passwordpara").removeClass("undo");
 });
 
