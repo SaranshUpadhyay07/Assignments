519c50f1c1a475c89c302cfb5a5e340ae6dd3782
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-26 03:34:27 +0530

    To do list V0.1

---
+++ Assignment 7/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+
+var items=[];
+
+app.get("/", function(req,res){
+    res.render("list",{ejes: items})
+});
+
+app.post('/', function(req,res){
+    var item = req.body.ele1;
+    items.push(item);
+    res.redirect("/")
+})
+
+app.listen(5023, function(){
+    console.log("Server started");
+});
