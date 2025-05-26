c31ed357e329e4aa6cea1c298c8367040f137947
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-27 03:41:33 +0530

    my list

---
+++ Assignment 7/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5023;
+
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+
+var items=[];
+
+app.get("/", function(req,res){
+    res.render("list",{ejes: items})
+});
+
+app.post('/', function(req,res){
+    var item = req.body.ele1;
+    items.push(item);
+    res.redirect("/")
+})
+
+app.listen(port, function(){
+    console.log("Server started");
+});
