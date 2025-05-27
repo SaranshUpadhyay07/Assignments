e62cd8d9a5c4323e840dec25efc40a304f8acedd
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-27 07:40:23 +0530

    FInal Todo list

--- Assignment 7/index.js
+++ Assignment 7/index.js
@@ -16,12 +16,42 @@ var low=[];
 app.get("/", function(req,res){
     res.render("list",{ejes: items})
 });
+app.get("/high", function(req,res){
+    res.render("list",{ejes: high})
+});app.get("/mid", function(req,res){
+    res.render("list",{ejes: medium})
+});app.get("/low", function(req,res){
+    res.render("list",{ejes: low})
+});
+app.get("/all", function(req,res){
+    res.render("list",{ejes: items})
+});
 
-app.post('/', function(req,res){
-    var item = req.body.ele1;
-    items.push(item);
-    res.redirect("/")
-})
+app.post('/', function (req, res) {
+    const item = req.body.ele1;
+    const priority = req.body.priority;
+
+    switch (priority) {
+        case "high":
+            high.push(item);
+            items.push(item); // also add to main items list
+            break;
+        case "medium":
+            medium.push(item);
+            items.push(item); // also add to main items list
+            break;
+        case "low":
+            low.push(item);
+            items.push(item); // also add to main items list
+            break;
+        default:
+            items.push(item); // no priority
+            break;
+    }
+
+    res.redirect("/");
+});
+
 
 app.listen(port, function(){
     console.log("Server started");
